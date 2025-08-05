c65930aca3c8a74f10c5be0926a2e6453f705d99
DevSandraSartori <elis.sartori@gmail.com>
2025-08-05 20:11:58 -0300

    Update n1_nc4.c

--- n1_nc4.c
+++ n1_nc4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(){
+    int idade;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "Sandra";
+
+    idade = 25;
+
+    quantidade = 10;  
+}
